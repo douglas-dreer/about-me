13c1131a39089ec9acb4673852dd091575142fef
Douglas Dreer <63859042+douglas-dreer@users.noreply.github.com>
2026-04-07 13:18:12 -0300

    Add files via upload
    Commit initial

---
+++ webpack.config.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'asserts', to: 'asserts' },
+        { from: 'asserts/img', to: 'assets/img' },
+        { from: 'asserts/curriculo-douglas-dreer.pdf', to: 'assets/pdf/curriculo.pdf' },
+        { from: 'css', to: 'css' },
+        { from: 'js/vendor', to: 'js/vendor' },
+        { from: 'icon.svg', to: 'icon.svg' },
+        { from: 'favicon.ico', to: 'favicon.ico' },
+        { from: 'robots.txt', to: 'robots.txt' },
+        { from: 'icon.png', to: 'icon.png' },
+        { from: '404.html', to: '404.html' },
+        { from: 'site.webmanifest', to: 'site.webmanifest' },
+      ],
+    }),
+  ],
+});
